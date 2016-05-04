4e9d6abd0c273fb0cad965691041bd78d17ce545
Zachary Perlmutter <zrp200@gmail.com>
2016-05-04 10:07:35 -0700

    Seeing if I can help

--- lib/the-wizards-castle/room_content.rb
+++ lib/the-wizards-castle/room_content.rb
@@ -1,102 +1,103 @@
 module TheWizardsCastle
-class RoomContent
+  class RoomContent
 
-  INT_TO_SYMBOL_MAP = {
-     0 => :unset, # 0 wasn't used in the original BASIC version
-     1 => :empty_room,
-     2 => :entrance,
-     3 => :stairs_up,
-     4 => :stairs_down,
-     5 => :magic_pool,
-     6 => :chest,
-     7 => :gold,
-     8 => :flares,
-     9 => :warp,
-    10 => :sinkhole,
-    11 => :crystal_orb,
-    12 => :book,
+    INT_TO_SYMBOL_MAP = {
+      0 => :unset, # 0 wasn't used in the original BASIC version
+      1 => :empty_room,
+      2 => :entrance,
+      3 => :stairs_up,
+      4 => :stairs_down,
+      5 => :magic_pool,
+      6 => :chest,
+      7 => :gold,
+      8 => :flares,
+      9 => :warp,
+      10 => :sinkhole,
+      11 => :crystal_orb,
+      12 => :book,
                        # monsters
-    13 => :kobold,
-    14 => :orc,
-    15 => :wolf,
-    16 => :goblin,
-    17 => :ogre,
-    18 => :troll,
-    19 => :bear,
-    20 => :minotaur,
-    21 => :gargoyle,
-    22 => :chimera,
-    23 => :balrog,
-    24 => :dragon,
-    25 => :vendor,       #not a monster
+      13 => :kobold,
+      14 => :orc,
+      15 => :wolf,
+      16 => :goblin,
+      17 => :ogre,
+      18 => :troll,
+      19 => :bear,
+      20 => :minotaur,
+      21 => :gargoyle,
+      22 => :chimera,
+      23 => :balrog,
+      24 => :dragon,
+      25 => :vendor,       #not a monster
                        #treasures
-    26 => :ruby_red,                #cure lethargy
-    27 => :norn_stone,
-    28 => :pale_pearl,              #cure leech
-    29 => :opal_eye,                #cure blindness
-    30 => :green_gem,               #cure forgetfulness
-    31 => :blue_flame,              #dissolves books
-    32 => :palantir,
-    33 => :silmaril,
+      26 => :ruby_red,                #cure lethargy
+      27 => :norn_stone,
+      28 => :pale_pearl,              #cure leech
+      29 => :opal_eye,                #cure blindness
+      30 => :green_gem,               #cure forgetfulness
+      31 => :blue_flame,              #dissolves books
+      32 => :palantir,
+      33 => :silmaril,
 
     # the following were not implemented like this in BASIC, but it'll be better this way
-    34 => :orb_of_zot,
-    35 => :runestaff_and_monster
-  }
+      34 => :orb_of_zot,
+      35 => :runestaff_and_monster
+    }
 
-  ROOM_DISPLAY_CHARACTERS = '?.EUDPCGFWSOBMMMMMMMMMMMMVTTTTTTTTWM'
+    ROOM_DISPLAY_CHARACTERS = '?.EUDPCGFWSOBMMMMMMMMMMMMVTTTTTTTTWM'
                            # 0123456789 123456789 123456789 12345'
 
-  @@symbol_to_intcode_map = INT_TO_SYMBOL_MAP.map{|k,v| [v,k]}.to_h
+    @@symbol_to_intcode_map = INT_TO_SYMBOL_MAP.map{|k,v| [v,k]}.to_h
+
+    class << self
+      def valid_intcode?(intcode)
+        INT_TO_SYMBOL_MAP.has_key?(intcode)
+      end
+
+      def valid_symbol?(symbol)
+        @@symbol_to_intcode_map.has_key?(symbol)
+      end
+
+      def to_symbol(intcode)
+        INT_TO_SYMBOL_MAP[intcode] or raise "Unrecognized intcode"
+      end
+      alias to_sym to_symbol
+
+      def to_intcode(symbol)
+        @@symbol_to_intcode_map[symbol] or raise "Unrecognized symbol"
+      end
+    end
 
 
-  def self.valid_intcode?(intcode)
-    INT_TO_SYMBOL_MAP.has_key?(intcode)
-  end
+    attr_reader :intcode
 
-  def self.valid_symbol?(symbol)
-    @@symbol_to_intcode_map.has_key?(symbol)
-  end
+    def initialize(intcode, curses = Hash.new false)
+      raise "Unrecognized intcode" unless RoomContent.valid_intcode?(intcode)
+      @intcode = intcode
+      @cursed_with_lethargy = curses[:lethargy]
+      @cursed_with_leech = curses[:leech]
+      @cursed_with_forgetfulness = curses[:forget]
+    end
 
-  def self.to_symbol(intcode)
-    INT_TO_SYMBOL_MAP[intcode] or raise "Unrecognized intcode"
-  end
+    def symbol
+      RoomContent.to_symbol(@intcode)
+    end
 
-  def self.to_intcode(symbol)
-    @@symbol_to_intcode_map[symbol] or raise "Unrecognized symbol"
-  end
+    def display
+      ROOM_DISPLAY_CHARACTERS[@intcode]
+    end
 
+    def cursed_with_lethargy?
+      @cursed_with_lethargy
+    end
 
+    def cursed_with_leech?
+      @cursed_with_leech
+    end
 
-  attr_reader :intcode
-
-  def initialize(intcode,curse_lethargy=false,curse_leech=false,curse_forget=false)
-    raise "Unrecognized intcode" unless RoomContent.valid_intcode?(intcode)
-    @intcode = intcode
-    @cursed_with_lethargy = curse_lethargy
-    @cursed_with_leech = curse_leech
-    @cursed_with_forgetfulness = curse_forget
-  end
-
-  def symbol
-    RoomContent.to_symbol(@intcode)
-  end
-
-  def display
-    ROOM_DISPLAY_CHARACTERS[@intcode]
-  end
-
-  def cursed_with_lethargy?
-    @cursed_with_lethargy
-  end
-
-  def cursed_with_leech?
-    @cursed_with_leech
-  end
-
-  def cursed_with_forgetfulness?
-    @cursed_with_forgetfulness
-  end
+    def cursed_with_forgetfulness?
+      @cursed_with_forgetfulness
+    end
 
 end
 end
